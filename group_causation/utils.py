a1ee7eb6c8dcff6592e8a021684013e4308d3d63
Joaquín Mateos Barroso <jmateosbarroso@gmail.com>
2026-05-22 09:48:04 +0200

    updated logging options

--- group_causation/utils.py
+++ group_causation/utils.py
@@ -329,3 +329,40 @@ def window_to_summary_graph(window_graph: dict[int, list[tuple[int, int]]]) -> d
                 summary_graph[son].append(summary_edge)
                 
     return summary_graph
+
+
+''''
+    LOGGING UTILITIES
+'''
+import logging
+
+def configure_logging(info_file=None, debug_file=None):
+    """
+    Configure the logger 'group-causation'.
+    - debug_file: Capture DEBUG and all the higher levels (INFO, WARNING, etc.)
+    - info_file: Capture INFO and all the higher levels (WARNING, etc.)
+    """
+    logger = logging.getLogger('group-causation')
+    
+    # The root logger level is set to DEBUG to ensure all messages are processed
+    logger.setLevel(logging.DEBUG)
+    
+    # 1. Configuration of the file for DEBUG (and all higher levels)
+    if debug_file:
+        fh_debug = logging.FileHandler(debug_file)
+        fh_debug.setLevel(logging.DEBUG)
+        # Formato opcional para que sea legible
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh_debug.setFormatter(formatter)
+        logger.addHandler(fh_debug)
+
+    # 2. Configuration of the file for INFO (and all higher levels)
+    if info_file:
+        fh_info = logging.FileHandler(info_file)
+        fh_info.setLevel(logging.INFO)
+        fh_info.setFormatter(formatter)
+        logger.addHandler(fh_info)
+
+    # 3. Avoid propagation to root logger to prevent duplicate logs in console
+    if not logger.handlers:
+        logger.addHandler(logging.NullHandler())
